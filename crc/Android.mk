7508265bb1891b0d2fa643d4f1798ebd380d0d1f
yhx <yhx@rock-chips.com>
2014-12-27 21:51:00 +0800

    add support for 64bit

--- crc/Android.mk
+++ crc/Android.mk
@@ -1,6 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-	
-LOCAL_PREBUILT_LIBS :=libcrc32.a
+
+ifeq "$(TARGET_ARCH)" "arm64"
+LOCAL_PREBUILT_LIBS :=lib64/libcrc32.a
+else ifeq "$(TARGET_ARCH)" "arm"
+LOCAL_PREBUILT_LIBS :=lib/libcrc32.a
+endif
 
 include $(BUILD_MULTI_PREBUILT)
